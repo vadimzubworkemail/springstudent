870da01cbc5a16c7f4123b4e32b09cdceebf249d
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2024-01-03 10:41:48 +0300

    First commit

---
+++ src/test/java/ru/corse/springstudent/SpringstudentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.corse.springstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
